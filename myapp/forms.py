117a07275cb78898119e44d433d45fd05274b267
Nestor <ndr1970@gmail.com>
2020-11-14 19:36:53 -0300

    CSS styles and Readme.MD updated

--- myapp/forms.py
+++ myapp/forms.py
@@ -6,12 +6,13 @@ from django import forms
 class ContactForm(forms.Form):
     """
      Specify as variables, the fields that compose the Contact form.
+     Visit https://docs.djangoproject.com/en/3.1/topics/forms/#building-a-form-in-django
     """
     name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter your name here'}),
-                           max_length=50, required=False)
+                           label='Your name', max_length=50, required=False)
     subject = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter subject here'}),
-                              max_length=150, required=True)
+                              label='Subject', max_length=150, required=True)
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Enter your email here'}),
-                             max_length=70, required=True)
+                             label='Your email', max_length=70, required=True)
     message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Enter your message here'}),
-                              max_length=500, required=True)
+                              label='Your Message', max_length=500, required=True)
